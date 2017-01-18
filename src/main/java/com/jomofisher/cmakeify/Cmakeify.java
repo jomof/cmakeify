627f0ce7095b703105bdf3866b0f46a4ea11e929
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-01-17 19:34:59 -0800

    Yaml

--- src/main/java/com/jomofisher/cmakeify/Cmakeify.java
+++ src/main/java/com/jomofisher/cmakeify/Cmakeify.java
@@ -1,7 +1,10 @@
 package com.jomofisher.cmakeify;
 
+import org.yaml.snakeyaml.Yaml;
+
 public class Cmakeify {
     public static void main(String [] args) {
+        Yaml yaml = new Yaml();
         System.out.printf("CMakeify!!!\n");
     }
 }
