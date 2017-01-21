2ed446c0193cad5abed67ec3902e925a59f260e0
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-01-21 13:45:36 -0800

    Initial generation

--- src/main/java/com/jomofisher/cmakeify/ArchiveInfo.java
+++ src/main/java/com/jomofisher/cmakeify/ArchiveInfo.java
@@ -22,13 +22,18 @@ public class ArchiveInfo {
     public String downloadToFolder(String downloadFolder) {
         StringBuilder sb = new StringBuilder();
         sb.append(String.format("if [ ! -f %s/%s ]; then\n", downloadFolder, name));
-        sb.append(String.format("    wget --no-check-certificate %s -O %s/%s &> %s/%s.log\n",
+        sb.append(String.format("    wget --no-check-certificate %s -O %s/%s &> %s/%s.download-log\n",
                 url, downloadFolder, name, downloadFolder, name));
         sb.append("fi");
         return sb.toString();
     }
 
     public String uncompressToFolder(String downloadFolder, String toolsFolder) {
-        return String.format("tar xvfz %s/%s -C %s", downloadFolder, name, toolsFolder);
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("if [ ! -d %s/%s ]; then\n", toolsFolder, baseName));
+        sb.append(String.format("    tar xvfz %s/%s -C %s &> %s/%s.uncompress-log\n",
+                downloadFolder, name, toolsFolder, toolsFolder, baseName));
+        sb.append("fi");
+        return sb.toString();
     }
 }
