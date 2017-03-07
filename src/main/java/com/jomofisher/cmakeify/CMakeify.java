2aa384dcb0c24ee6b78a31ee02f40b5958042b88
Jomo Fisher <jomof@google.com>
2017-03-07 12:59:06 -0800

    Mac/iOS support

--- src/main/java/com/jomofisher/cmakeify/CMakeify.java
+++ src/main/java/com/jomofisher/cmakeify/CMakeify.java
@@ -156,9 +156,6 @@ public class CMakeify {
             for (String cmakeVersion : config.cmake.versions) {
                 switch (target) {
                     case android:
-                      if (config.android.lib == null || config.android.lib.length() == 0) {
-                        throw new RuntimeException("Expected android.lib");
-                      }
                         for (String ndk : config.android.ndk.versions) {
                             RemoteArchive remote = config.android.ndk.remotes.get(ndk);
                             if (remote == null) {
@@ -224,9 +221,6 @@ public class CMakeify {
                             String toolchainRepo = config.iOS.cmakeToolchainRemotes
                                 .get(cmakeToolchain);
                             script.gitClone(cmakeToolchain, toolchainRepo);
-                          if (config.iOS.lib == null || config.iOS.lib.length() == 0) {
-                            throw new RuntimeException("Expected iOS.lib");
-                          }
                             for (iOSPlatform platform : config.iOS.platforms) {
                               Map<String, String> flavors = config.iOS.flavors;
                                 if (flavors == null) {
