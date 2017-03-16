bd750f2ba1b0eb86be0e3642deeb1251b730de31
Jomo Fisher (windows) <jomofisher@gmail.com>
2017-03-15 21:42:32 -0700

    iOS

--- src/main/java/com/jomofisher/cmakeify/BashScriptBuilder.java
+++ src/main/java/com/jomofisher/cmakeify/BashScriptBuilder.java
@@ -656,7 +656,8 @@ public class BashScriptBuilder extends ScriptBuilder {
     String user = targetGroupId.substring(targetGroupId.lastIndexOf(".") + 1);
 
     body(
-        "%s/github-release upload --user %s --repo %s --tag %s --name %s --file %s",
+        "%s/%s/github-release upload --user %s --repo %s --tag %s --name %s --file %s",
+        TOOLS_FOLDER,
         getHostArchive(githubRelease).unpackroot,
         user,
         targetArtifactId,
